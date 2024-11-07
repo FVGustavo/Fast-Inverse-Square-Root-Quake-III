8c4e9178225a60abbc512f50b99bd14807a850f1
Gustavo Ferreira Vieira <97406828+FVGustavo@users.noreply.github.com>
2024-11-07 15:21:11 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,5 @@
 #include <stdio.h>
-
-// Quake III Arena players function
-float Q_rsqrt( float number ){
-	long i;
-	float x2, y;
-	const float threehalfs = 1.5F;
-
-	x2 = number * 0.5F;
-	y  = number;
-	i  = * ( long * ) &y;						// evil floating point bit level hacking
-	i  = 0x5f3759df - ( i >> 1 );               // what the fuck?
-	y  = * ( float * ) &i;
-	y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
-	y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
-
-	return y;
-}
+#include "fast_inverse_sqrt.h"
 
 int main(){
     float number = 0;
