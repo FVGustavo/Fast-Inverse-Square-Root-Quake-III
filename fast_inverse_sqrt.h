745bc8d580350bd396ee5fb573813f4d061492fe
Gustavo Ferreira Vieira <97406828+FVGustavo@users.noreply.github.com>
2024-11-07 15:22:27 -0300

    Create fast_inverse_sqrt.h

---
+++ fast_inverse_sqrt.h
@@ -0,0 +1,7 @@
+/**
+ * @brief function developed by Quake III Arena players to calculate the inverse square root of a given number
+ * 
+ * @param number 
+ * @return float 
+ */
+float Q_rsqrt(float number);
